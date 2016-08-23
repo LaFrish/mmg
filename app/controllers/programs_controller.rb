0c9a8138d23b0c8333c60bfd20b5093562c761bd
YOUR FULL NAME <farishta.haider@gmail.com>
2016-08-23 17:12:38 -0400

    Added program controller

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -5,9 +5,13 @@ class ProgramsController < ApplicationController
   end
 
   def new
+    @programs = Program.new
   end
 
   def create
+    @programs = Program.create!(program_params)
+
+    redirect_to programs_path(@program)
   end
 
   def show
@@ -15,16 +19,25 @@ class ProgramsController < ApplicationController
   end
 
   def edit
+    @program = Program.find(params[:id])
   end
 
   def update
+    @program = Program.find(params[:id])
+    @program.update(program_params)
+
+    redirect_to programs_path(@program)
   end
 
   def destroy
+    @program = Program.find(params[:id])
+    @program.destroy
+
+    redirect_to programs_path
   end
 
   private
-  def _params
-    params.require(:).permit()
+  def program_params
+    params.require(:program).permit()
   end
 end
